5f4e9d9e0be4280a93fc8032e6754ddb16bc58c6
David Torres <detour27@gmail.com>
2025-12-13 12:33:44 -0500

    update pdfjs (#91)

--- public/js/pdf-viewer.js
+++ public/js/pdf-viewer.js
@@ -33,7 +33,7 @@
     const setWorker = () => {
       if (pdfjsLib.GlobalWorkerOptions) {
         pdfjsLib.GlobalWorkerOptions.workerSrc =
-          'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.2.67/pdf.worker.min.mjs';
+          'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs';
       }
     };
 
